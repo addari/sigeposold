4f5074c6be117d1e3e67a1371497fa2f4e589aa4
Luciano Addari <addari@gmail.com>
2013-02-14 12:02:06 -0500

    Cambio en las Conexiones de Base de Datos

--- web/sige_prod.php
+++ web/sige_prod.php
@@ -3,5 +3,5 @@
 
 require_once(dirname(__FILE__).'/../config/ProjectConfiguration.class.php');
 
-$configuration = ProjectConfiguration::getApplicationConfiguration('sige', 'hosting', false);
+$configuration = ProjectConfiguration::getApplicationConfiguration('sige', 'prod', false);
 sfContext::createInstance($configuration)->dispatch();
